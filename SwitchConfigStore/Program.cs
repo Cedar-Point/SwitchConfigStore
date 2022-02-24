ff74545b6fad4680b82937a8a262d4b90795144f
dylan.bickerstaff <dylan.bickerstaff@CP-LT-1232>
2022-02-24 12:55:47 -0500

    Add project files.

---
+++ SwitchConfigStore/Program.cs
@@ -0,0 +1,21 @@
+using SwitchConfigStore;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<TFTPServer>();
+var app = builder.Build();
+app.Services.GetService<TFTPServer>();
+app.MapGet("/{IPAddress}", async (con) => {
+    StreamWriter sw = new StreamWriter(con.Response.Body);
+    string IP = (string)con.Request.RouteValues["IPAddress"];
+    TFTPServer server = con.RequestServices.GetService<TFTPServer>();
+    if (server.Configs.ContainsKey(IP))
+    {
+        con.Response.Body.Write(server.Configs[IP].ToArray());
+    }
+    else
+    {
+        sw.WriteLine("Config not found.");
+        await sw.FlushAsync();
+    }
+    await con.Response.Body.FlushAsync();
+});
+app.Run();
---
+++ SwitchConfigStore/TFTPServer.cs
@@ -0,0 +1,26 @@
+﻿using Tftp.Net;
+using System.Net;
+
+namespace SwitchConfigStore
+{
+    public class TFTPServer
+    {
+        public Dictionary<string, MemoryStream> Configs = new();
+        public TftpServer Server = new();
+        public TFTPServer()
+        {
+            Server.Start();
+            Server.OnWriteRequest += Server_OnWriteRequest;
+        }
+        private void Server_OnWriteRequest(ITftpTransfer transfer, EndPoint client)
+        {
+            transfer.UserContext = new KeyValuePair<string, MemoryStream>(((IPEndPoint)client).Address.ToString(), new());
+            transfer.Start(((KeyValuePair<string, MemoryStream>)transfer.UserContext).Value);
+            transfer.OnFinished += Transfer_OnFinished;
+        }
+        private void Transfer_OnFinished(ITftpTransfer transfer)
+        {
+            Configs[((KeyValuePair<string, MemoryStream>)transfer.UserContext).Key] = ((KeyValuePair<string, MemoryStream>)transfer.UserContext).Value;
+        }
+    }
+}
